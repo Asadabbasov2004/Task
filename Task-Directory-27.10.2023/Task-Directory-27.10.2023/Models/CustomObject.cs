1405435ef6350aecc9e1133b66a3d89dbd4d7b99
II novbe <II novbe@DESKTOP-4EOUDP3>
2023-10-27 18:18:57 +0400

    uptated

---
+++ Task-Directory-27.10.2023/Task-Directory-27.10.2023/Models/CustomObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Directory_27._10._2023.Models
+{
+    internal  class CustomObject
+    {
+       
+    }
+}
---
+++ Task-Directory-27.10.2023/Task-Directory-27.10.2023/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using Task_Directory_27._10._2023.Models;
+
+namespace Task_Directory_27._10._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\II novbe\Desktop\Task\Task-Directory-27.10.2023\Task-Directory-27.10.2023\";
+
+            Directory.CreateDirectory(path+@"\Models");
+            Directory.CreateDirectory(path+@"\Data");
+
+            if (!File.Exists(path + @"\Data\jsonDAta.json"))
+            {
+                File.Create(path + @"\Data\jsonDAta.json");
+            }
+            HttpClient httpClient = new HttpClient();
+            var result = httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/posts");
+
+            List<CustomObject> customObjects = JsonConvert.DeserializeObject<List<CustomObject>>(result);
+        }
+    }
+}
