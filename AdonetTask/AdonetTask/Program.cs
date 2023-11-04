437b2e908c12e70f393ac53f80ed9e748863e14c
Asadabbasov2004 <asadabbasov2004@gmail.com>
2023-11-04 23:28:20 +0400

    created first adonet task

---
+++ AdonetTask/AdonetTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdonetTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
